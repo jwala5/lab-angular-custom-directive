adf738afbc0b9b5668ab1cc6e5115d26dfe95d15
jwala5 <dubeyshiva98@gmail.com>
2020-04-30 03:36:49 +0530

    FSJO320A004

---
+++ custom-directive/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ColorChangeDirective } from './app.directive';
+@NgModule({
+  declarations: [
+    AppComponent,ColorChangeDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
